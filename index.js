49312f1aebe60734b79348d6f125e386689cd92f
mknyambura <kariuki.mercie@gmail.com>
2022-12-09 09:35:54 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+function submitData(userName, userEmail){
+  
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      userName,
+      userEmail,
+    })
+    .then(function (respone){
+      return Response.json();
+    })
+    .then(function (object){
+      console.log(object);
+    })
+    .catch( function(error) {
+      alert('ERROR!');
+      console.log(error.message);
+    })
+  });
+}
+
